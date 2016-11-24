3f514466c8d1a00b5eedab62e1057a3a0e41d59d
Yagyesh <yagyesh27@gmail.com>
2016-11-24 20:30:32 +0530

    FreeCompile Added

--- FinalProject/app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivityFragment.java
+++ FinalProject/app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivityFragment.java
@@ -6,8 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
+
 import com.udacity.gradle.builditbigger.R;
 
 
